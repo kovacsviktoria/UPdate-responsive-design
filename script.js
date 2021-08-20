0a3ee938848715dc5372039b42f587a926a31e62
Viktoria Toth <k.viki1117@gmail.com>
2021-08-20 16:00:38 +0200

    responsive navbar in progress

---
+++ script.js
@@ -0,0 +1,13 @@
+function loadEvent() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    }
+}
+
+window.addEventListener("load", loadEvent);
