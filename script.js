fce13fa2f899e50d1a5c78a5c838e9d82cfda3b2
Viktoria Toth <k.viki1117@gmail.com>
2021-08-20 16:26:23 +0200

    responsive navbar first version finished

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function loadEvent() {
-    const hamburger = document.querySelector(".hamburger");
+    const hamburger = document.querySelector(".hamburger"); //hamburger div element
     const navMenu = document.querySelector(".nav-menu");
+    //ul element
 
+    //a hamburger ikon kerul a click esemeny
     hamburger.addEventListener("click", mobileMenu);
 
-    function mobileMenu() {
-        hamburger.classList.toggle("active");
-        navMenu.classList.toggle("active");
+     function mobileMenu() {
+        hamburger.classList.toggle("active"); //kattintasra a hamburger ikon X lesz
+        navMenu.classList.toggle("active"); //kattintasra a menulista elemei lathatoak lesznek
+    }
+
+    const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach(n => n.addEventListener("click", closeMenu));
+    //
+    function closeMenu() {
+        hamburger.classList.remove("active"); //
+        navMenu.classList.remove("active");
     }
 }
 
